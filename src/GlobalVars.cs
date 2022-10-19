b64e4d0197cf223b62f976be89419dfc51774783
Scighost <scighost@outlook.com>
2022-10-19 16:37:35 +0800

    update

--- src/GlobalVars.cs
+++ src/GlobalVars.cs
@@ -17,8 +17,8 @@ public static class GlobalVars
     public static Logger.Level LogLevel = Logger.Level.Info;
     public static Version AppVersion = Assembly.GetEntryAssembly()!.GetName().Version!;
 
-    public const uint AppVersionCode = 29;
-    public const string AppVersionName = "2.1";
+    public const uint AppVersionCode = 30;
+    public const string AppVersionName = "2.2";
     public const string LibName = "YaeLib.dll";
     public const string PipeName = "YaeAchievementPipe";
     public const string BucketHost = "https://cn-cd-1259389942.file.myqcloud.com";
--- src/Program.cs
+++ src/Program.cs
@@ -11,7 +11,8 @@ CheckGenshinIsRunning();
 Console.WriteLine("----------------------------------------------------");
 Console.WriteLine($"YaeAchievement (Modified by Xunkong) - 原神成就导出工具 ({GlobalVars.AppVersionName})");
 Console.WriteLine("https://github.com/xunkong/YaeAchievement");
-Console.WriteLine("原项目 https://github.com/HolographicHat/YaeAchievement");
+Console.WriteLine("若出现原神闪退的情况，请从下方链接处下载最新版本。");
+Console.WriteLine("https://github.com/HolographicHat/YaeAchievement");
 Console.WriteLine("----------------------------------------------------");
 
 var gamePath = args.FirstOrDefault();
